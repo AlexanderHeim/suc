709dce424fb2b459a89b9f86da873d395ee78d33
alexanderheim <alexanderheimat@gmail.com>
2021-05-30 17:53:16 +0200

    fully implemented basic-capabilities

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,8 @@
 use suc::sucfile::SucFile;
 
 fn main() {
+    let mut sf = SucFile::open("test.suc").unwrap();
+    sf.add("Alexander", "4312541").unwrap();
+    //sf.remove("Alexander").unwrap();
+    println!("{}", sf.check("Alexander", "4312541").unwrap());
 }
