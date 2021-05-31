238d587008e3a7a8db0b1a5d53daa77a223b47a5
alexanderheim <alexanderheimat@gmail.com>
2021-05-31 19:07:10 +0200

    refactor

--- src/bin/main.rs
+++
@@ -1,8 +0,0 @@
-use suc::sucfile::SucFile;
-
-fn main() {
-    let mut sf = SucFile::open("test.suc").unwrap();
-    sf.add("Alexander", "4312541").unwrap();
-    //sf.remove("Alexander").unwrap();
-    println!("{}", sf.check("Alexander", "4312541").unwrap());
-}
